de42ed4375198d086d205aee41cca655f854c03d
Jarvis <wanyf@connect.hku.hk>
2022-07-17 22:51:53 +0800

    init

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,10 @@
 const assert = require('assert');
 const sayHello = require('../index');
-const getTheFirstLetterMatched = require('../test/demo')
 
 describe('index', () => {
-  describe('demo', () => {
-    it('test', () => {
-      let board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]];
-      let word = "ABCCED";
-      assert.deepEqual(getTheFirstLetterMatched(board, word), [0, 1])
+  describe('sayHello', () => {
+    it('should return "Hello"', () => {
+      assert.equal(sayHello(), "Hello")
     })
   })
 })
